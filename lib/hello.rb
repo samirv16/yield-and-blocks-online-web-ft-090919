24739a54f58db6ce75af34584e9ce63c1e54bfca
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-12 04:34:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+ if block_given?
   i = 0 
   
   while i < array.length 
@@ -7,11 +8,9 @@ def hello_t(array)
 end
 array
 
-end
+else
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+puts "Hey! No block was given!"
   end
 end
 
